75ced0bf0fc9a27abc0e279bd69f1ee78ea7a967
William Robersone <william.robersone@gmail.com>
2020-10-25 13:05:35 -0700

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by William Robersone on 10/25/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
